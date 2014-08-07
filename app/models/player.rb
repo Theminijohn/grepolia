ee54d6b2d44202774597f4cfba31d9c826c62ee4
The Mini John <admin@phoenix-prime.gr>
2014-08-07 21:45:23 +0300

    Change to only version update actions

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,7 +2,7 @@ class Player < ActiveRecord::Base
 
 	self.primary_key = 'grepo_id'
 
-	has_paper_trail on: [:create, :update], ignore: [:grepo_id, :name] 
+	has_paper_trail on: :update, ignore: [:grepo_id, :name] 
 
 	has_many :towns
 	belongs_to :alliance 
--- app/models/town.rb
+++ app/models/town.rb
@@ -2,7 +2,7 @@ class Town < ActiveRecord::Base
 
 	self.primary_key = 'grepo_id'
 
-	has_paper_trail on: [:create, :update], only: [:player_id, :name, :points] 
+	has_paper_trail on: :update, only: [:player_id, :name, :points] 
 	
 	belongs_to :player, foreign_key: :player_id
 
